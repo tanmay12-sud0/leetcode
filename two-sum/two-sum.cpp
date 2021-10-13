da24647f32aaa543abebbcb6e89c3f2f6d2be2c5
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-10-13 16:33:57 +0530

    Time: 42 ms (38.15%), Space: 11.9 MB (16.07%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,20 +1,60 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int, int>umap;
+        unordered_map<int, int>umap;
         vector<int>tan;
         for(int i=0; i<nums.size(); i++){
-           if(umap.count(target-nums[i])){
-               tan.push_back(umap[target-nums[i]]);
-               tan.push_back(i);
-           }else{
-               umap[nums[i]] = i;
-           }
+            if(umap.count(target-nums[i])){
+                tan.push_back(i);
+                tan.push_back(umap[target-nums[i]]);
+            }else{
+                umap[nums[i]] = i;
+            }
         }
-        
         return tan;
     }
 };
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// unordered_map<int, int>umap;
+//         vector<int>tan;
+//         for(int i=0; i<nums.size(); i++){
+//            if(umap.count(target-nums[i])){
+//                tan.push_back(umap[target-nums[i]]);
+//                tan.push_back(i);
+//            }else{
+//                umap[nums[i]] = i;
+//            }
+//         }
+        
+//         return tan;
