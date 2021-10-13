702d0a5c7cdb648249c5b869636e4dfb6e0edbc3
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-10-13 16:57:18 +0530

    Time: 18 ms (54.20%), Space: 8.4 MB (57.45%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,22 +2,106 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
        unordered_map<char, int>hash;
-        int cnt = 0;
-        int max_len =0;
-        int start =0;
+        int max_so_far = 0;
+        int maxs= 0;
+        int len = 0;
+        int start = 0;
         for(int i=0; i<s.length(); i++){
             if(hash.count(s[i]) and hash[s[i]] >= start){
-                start = hash[s[i]] + 1;
-                cnt= i-hash[s[i]];
+                max_so_far = max(max_so_far, maxs);
+                start = hash[s[i]]+1;
+            
+                maxs = i-hash[s[i]];
                 hash[s[i]] = i;
-                max_len = max(max_len, cnt);
             }else{
                 hash[s[i]] = i;
-                cnt++;
-                max_len = max(max_len, cnt);
+                maxs++;
+                max_so_far = max(max_so_far, maxs);
             }
         }
-        max_len = max(max_len, cnt);
-        return max_len;
+        max_so_far = max(max_so_far, maxs);
+        return max_so_far;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// unordered_map<char, int>hash;
+//         int cnt = 0;
+//         int max_len =0;
+//         int start =0;
+//         for(int i=0; i<s.length(); i++){
+//             if(hash.count(s[i]) and hash[s[i]] >= start){
+//                 start = hash[s[i]] + 1;
+//                 cnt= i-hash[s[i]];
+//                 hash[s[i]] = i;
+//                 max_len = max(max_len, cnt);
+//             }else{
+//                 hash[s[i]] = i;
+//                 cnt++;
+//                 max_len = max(max_len, cnt);
+//             }
+//         }
+//         max_len = max(max_len, cnt);
+//         return max_len;
