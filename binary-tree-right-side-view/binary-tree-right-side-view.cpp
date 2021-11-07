fa141d9bf8d1a6c7cc46818091975e340d29a062
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-11-07 02:56:24 -0500

    Time: 4 ms (63.81%), Space: 11.9 MB (89.18%) - LeetHub

---
+++ binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    
+    void rightView(TreeNode* root, int level, int &maxlevel, vector<int>&hash){
+	if(root == NULL){
+		return;
+	}
+	if(maxlevel < level){
+		hash.push_back(root->val);
+		maxlevel = level;
+	}
+
+	rightView(root->right, level+1, maxlevel, hash);
+	rightView(root->left, level+1, maxlevel, hash);
+	return;
+}
+    
+    
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>hash;
+        int maxlevel = -1;
+        rightView(root, 0,maxlevel, hash);
+        return hash;
+    }
+};
