f5145c36e0edad47f53452b2958bac814dca6147
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-11-19 00:50:42 +0530

    Time: 4 ms (54.91%), Space: 8.2 MB (12.83%) - LeetHub

---
+++ binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,43 @@
+
+class Solution {
+public:
+    
+    vector<int>hash;
+    
+    void inorder(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        
+        inorder(root->left);
+        hash.push_back(root->val);
+        inorder(root->right);
+        return;
+    }
+    int i=0;
+    void inorderchange(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+       
+        inorderchange(root->left);
+         root->val = hash[i++];
+        // hash.push_back(root->val);
+        inorderchange(root->right);
+        return;
+    }
+    
+    
+    TreeNode* bstToGst(TreeNode* root) {
+        inorder(root);
+        int n = hash.size();
+        for(int i= hash.size()-2; i>=0; i--){
+            hash[i] += hash[i+1];
+        }
+        for(auto x : hash){
+            cout << x << "< ";
+        }
+        inorderchange(root);
+        return root;
+    }
+};
