8ece57207016e5006e590cc4189e1ad45ea271f4
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-11-20 15:03:13 +0530

    Time: 84 ms (92.95%), Space: 59.6 MB (94.10%) - LeetHub

--- deepest-leaves-sum/deepest-leaves-sum.cpp
+++ deepest-leaves-sum/deepest-leaves-sum.cpp
@@ -1,29 +1,48 @@
 
 class Solution {
 public:
-    
-    int sum = 0;
-    
-    void sumD(TreeNode* root, int level, int &maxLevel){
-        if(root==NULL){
-            return;
-        }
-        if(maxLevel == level){
-            sum += root->val;
-        }else if(level>maxLevel){
-            sum = root->val;
-            maxLevel = level;
-        }
-        sumD(root->left, level+1, maxLevel);
-        sumD(root->right, level+1, maxLevel);
-        return;
-        
-    }
+    int deepest = 0, sum = 0;
+int deepestLeavesSum(TreeNode* root, int depth = 0) {
+	if(!root) return 0;  // current node is NULL - just return.
+	if(!root -> left && !root -> right) // if current is a leaf node
+		if(depth == deepest) sum += root -> val;  // if depth is exactly equal to deepest, add to sum
+		else if(depth > deepest) sum = root -> val, deepest = depth; // found a more deeper leaf node, reset sum and update deepest
+	deepestLeavesSum(root -> left, depth + 1);   // recurse for the left, 
+	deepestLeavesSum(root -> right, depth + 1);  // and the right node of the current node
+	return sum; // finally return the sum accumulated till now which will be sum of value of deepest leaves
+}
 
     
-    int deepestLeavesSum(TreeNode* root) {
-        int maxLevel = -1;
-        sumD(root, 0, maxLevel);
-        return sum;
-    }
 };
+
+
+
+
+
+
+
+
+
+// int sum = 0;
+    
+//     void sumD(TreeNode* root, int level, int &maxLevel){
+//         if(root==NULL){
+//             return;
+//         }
+//         if(maxLevel == level){
+//             sum += root->val;
+//         }else if(level>maxLevel){
+//             sum = root->val;
+//             maxLevel = level;
+//         }
+//         sumD(root->left, level+1, maxLevel);
+//         sumD(root->right, level+1, maxLevel);
+//         return; 
+//     }
+
+    
+//     int deepestLeavesSum(TreeNode* root) {
+//         int maxLevel = -1;
+//         sumD(root, 0, maxLevel);
+//         return sum;
+//     }
