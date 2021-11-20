08cb840c0679376cf5e82926e7e741a60a9862bc
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-11-20 10:55:31 +0530

    Time: 4 ms (70.46%), Space: 13.5 MB (8.73%) - LeetHub

---
+++ sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+    int sum = 0;
+    void leftsum(TreeNode* root, bool level){
+        if(root==NULL){
+            
+            return;
+        }
+        if(level == true and root->left==NULL and root->right==NULL){
+            sum += root->val;
+            
+        }
+        leftsum(root->left, true);
+        leftsum(root->right, false);
+        return;
+    }
+
+    
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        int maxLevel = -1;
+        leftsum(root, false);
+        return sum;
+    }
+};
