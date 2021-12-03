e889eb6723fb098d4fb85eb0432a84ef905adc0f
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-12-03 12:26:45 +0530

    Time: 52 ms (64.41%), Space: 26.1 MB (66.82%) - LeetHub

---
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+    bool hogya(vector<int>& weights, int days, int mid, int n){
+        int sum = 0;
+        int count = 0;
+        for(int i=0; i<n;i++){
+            sum += weights[i];
+            if(weights[i]>mid){
+                return false;
+            }
+            if(sum > mid){
+                // cout << sum << endl;
+                count++;
+                sum = weights[i];
+            
+            }
+        }
+        // cout << "tan" << count << endl;
+        if(count < days){
+            return true;
+        }
+       return false;
+    }
+    
+    
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int total_weight = 0;
+        int n = weights.size();
+        for(int i=0; i<n; i++){
+            total_weight += weights[i];
+        }
+        int start = 1;
+        int ans  = 0;
+        int end = total_weight;
+        while(start <= end){
+            int mid = (start+end)/2;
+            // cout << mid << endl;
+            if(hogya(weights, days,mid, n)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+};
