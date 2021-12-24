0e2a8d78f51c16f26e7e482faa61c78514d45053
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2021-12-24 09:22:12 +0530

    Time: 20 ms (62.64%), Space: 14.2 MB (81.35%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -5,17 +5,15 @@ public:
         if(len<=1)
             return intervals;
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> res; // result vector
-        // insert the first element into the result vector
+        vector<vector<int>> res;
+       
         res.push_back(intervals[0]);
         for(int i=1;i<len;i++){
             if(res.back()[1]>=intervals[i][0])
-                // back() points to the final element of the vector.
-                // Update the endpoint of final element of result 
-                // vector if there is an overlap with intervals[i]
+              
                 res.back()[1]=max(res.back()[1], intervals[i][1]);
             else
-                // If no overlap, insert intervals[i]
+              
                 res.push_back(intervals[i]);
         }
         return res;
