66a574a1f1a79235e2f17ed108041263bd103606
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-05 20:39:00 +0530

    Time: 20 ms (86.86%), Space: 14.1 MB (15.23%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>> >pq;
+        int n = lists.size();
+        for(int i=0; i<n; i++){
+            ListNode* dummy = lists[i];
+            if(dummy){
+                pq.push(make_pair(lists[i]->val, dummy));
+            }
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        while(!pq.empty()){
+            pair<int, ListNode*>pp = pq.top();
+            pq.pop();
+            ListNode* x = new ListNode(pp.first);
+            temp->next = x;
+            temp = x;
+            if(pp.second->next != NULL){
+                pq.push(make_pair(pp.second->next->val, pp.second->next));
+            }
+        }
+        return dummy->next;
+    }
+};
