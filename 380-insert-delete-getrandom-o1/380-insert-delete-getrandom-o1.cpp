c8caee06344752848e6a6c89caab742829641f6d
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-07 16:00:07 +0530

    Time: 318 ms (22.25%), Space: 96.9 MB (83.93%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,37 @@
+class RandomizedSet {
+public:
+    unordered_set<int>hash;
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(hash.count(val)){
+            return false;
+        }else{
+            hash.insert(val);
+            return true;
+        }
+    }
+    
+    bool remove(int val) {
+        if(hash.count(val)){
+            hash.erase(val);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    int getRandom() {
+        int len = hash.size();
+        int idx = rand()%len;
+        auto it = hash.begin();
+        for (int i = 0; i < idx; i++)
+        {
+            it++;
+        }
+        return *it;
+    }
+};
+
