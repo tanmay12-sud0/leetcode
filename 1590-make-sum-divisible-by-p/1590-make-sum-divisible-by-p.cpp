bc1b7960e8521c2340e3178e0d2c13e416a21f8a
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-09 13:04:06 +0530

    Time: 273 ms (19.69%), Space: 66.5 MB (70.00%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        int target = 0;
+        for(auto el: nums) target = (target + el) % p; // sum % k essentially
+        unordered_map<int, int> mp; // Our Hasmap
+        mp[0] = -1; // if we are looking for 0, we can take the complete subarray[0...i]
+        int cur = 0;
+        int ans = 1e9;
+        if(!target) return 0;
+        for(int i = 0; i<n; i++){
+            
+            cur = (cur + nums[i]) % p;
+            // we need to reach the target, lmao
+            int need = (cur - target + p) % p; // This is the element we are looking for
+            if(mp.find(need) != mp.end()){
+                ans = min(ans, i - mp[need]);
+            }
+            mp[cur] = i;  // hashing the current occurence as the latest occurence of cur % p
+        
+        }
+        
+        if(ans >= n) return -1; // we cannot delethe complete array, hence
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int minSubarray(vector<int>& nums, int p){
+        
+//         int sum = 0;
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
+//             sum += nums[i];
+//             sum %= p;
+//         }
+        
+//         if(sum == 0){
+//             return 0;
+//         }
+     
+//         int min_so_far = INT_MAX;
+//         unordered_map<int, int>hash;
+//         int pre = 0;
+//         hash[0] = -1;
+//         for(int i=0; i<n; i++){
+//             pre += nums[i];
+//             pre %= p;
+//             int x = (pre-sum+p)%p;
+//             if(hash.count(x)){
+//                 min_so_far = min(min_so_far, i-hash[x]);
+//             }
+//             hash[pre] = i;
+//         }
+//         if(min_so_far >= n){
+//             return -1;
+//         }
+//         return min_so_far;
+//     }
+// };
