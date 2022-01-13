fd1b74a26e380276ae96180c77ec92f85eb3a871
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-13 22:03:30 +0530

    Time: 461 ms (8.81%), Space: 144.5 MB (91.96%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+
+    void minDis(TreeNode* root, int level, int &mindep){
+        if(root == NULL){
+            return;
+        }
+        if(root->left==NULL and root->right==NULL){
+            if(level < mindep){
+                mindep = level;
+            }
+            return;
+        }
+        
+        minDis(root->left, level+1, mindep);
+        minDis(root->right, level+1, mindep);
+        return;
+    }
+    
+    
+    int minDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        int mindep = INT_MAX;
+        minDis(root, 1, mindep);
+        return mindep;
+    }
+};
