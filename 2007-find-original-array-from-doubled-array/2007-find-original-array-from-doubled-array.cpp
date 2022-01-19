9550e86419fd43962e9748f2fcae998cf85f67d7
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-19 11:54:28 +0530

    Time: 1103 ms (5.08%), Space: 136.5 MB (89.22%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,35 @@
+bool compare(int a, int b){
+    return a>b;
+}
+
+class Solution {
+public:
+    
+    vector<int> findOriginalArray(vector<int>& changed) {
+        vector<int>umap;
+        unordered_map<int, int>hash;
+        if(changed.size()%2!=0){
+            return umap;
+        }
+        sort(changed.begin(), changed.end(), compare);
+        for(int i=0; i<changed.size(); i++){
+            if(hash.count(2*changed[i])){
+                umap.push_back(changed[i]);
+                hash[2*changed[i]]--;
+                if(hash[2*changed[i]] == 0){
+                    hash.erase(2*changed[i]);
+                }
+            }else{
+                hash[changed[i]]++;
+            }
+        }
+        for(auto x : umap){
+            cout << x << ", ";
+        }
+        if(umap.size() == changed.size()/2){
+            return umap;
+        }
+        umap.clear();
+        return umap;
+    }
+};
