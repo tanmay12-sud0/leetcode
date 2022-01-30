843225206d9f67ce8df1adad8838d6d54ea093a9
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-30 09:33:36 +0530

    Time: 206 ms (42.91%), Space: 95.5 MB (51.40%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime) {
-        vector<pair<char, int>>ss;
-        int needTime = 0;
-        for(int i=0; i<colors.length(); i++){
-            if(ss.empty()){
-                ss.push_back(make_pair(colors[i], i));
-            }else{
-                if(ss.back().first == colors[i]){
-                    if(neededTime[ss.back().second] < neededTime[i]){
-                        needTime += neededTime[ss.back().second];
-                        ss.pop_back();
-                        ss.push_back(make_pair(colors[i], i));
-                    }else{
-                        needTime += neededTime[i];
-                    }
-                }else{
-                    ss.push_back(make_pair(colors[i], i));
-                }
-            }
+    int minCost(string s, vector<int>& cost) 
+    {
+        int res = 0;
+        int n = s.size();
+        for(int i = 1; i < n; i++)
+        {
+            if(s[i] == s[i - 1])
+            {
+                res += min(cost[i], cost[i - 1]); 
+                cost[i] = max(cost[i], cost[i - 1]); 
+            }     
         }
-        return needTime;
+        
+        return res;
+        
     }
+    // int minCost(string colors, vector<int>& neededTime) {
+    //     vector<pair<char, int>>ss;
+    //     int needTime = 0;
+    //     for(int i=0; i<colors.length(); i++){
+    //         if(ss.empty()){
+    //             ss.push_back(make_pair(colors[i], i));
+    //         }else{
+    //             if(ss.back().first == colors[i]){
+    //                 if(neededTime[ss.back().second] < neededTime[i]){
+    //                     needTime += neededTime[ss.back().second];
+    //                     ss.pop_back();
+    //                     ss.push_back(make_pair(colors[i], i));
+    //                 }else{
+    //                     needTime += neededTime[i];
+    //                 }
+    //             }else{
+    //                 ss.push_back(make_pair(colors[i], i));
+    //             }
+    //         }
+    //     }
+    //     return needTime;
+    // }
 };
