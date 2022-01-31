4b85c4542a301a3ffeda4057b339ffc7584aa47f
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-01-31 20:22:13 +0530

    Time: 15 ms (14.45%), Space: 13.1 MB (46.11%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -12,37 +12,5 @@ public:
             profit += sell - buy;
         }
         return profit;
-        // int amount = 0;
-        // int buy = -1;
-        // int sell = -1;
-        // int n = prices.size();
-        // if(prices.size()==1){
-        //     return 0;
-        // }
-        // for(int i=0; i<n; i++){
-        //     if(prices[i] < prices[i+1] and i==0 and sell == -1){
-        //         buy = prices[i];
-        //         continue;
-        //     }
-        //     if(i!=0 and i!=n-1 and prices[i]<prices[i+1] and prices[i]<prices[i-1] and sell == -1 and buy == -1){
-        //         buy = prices[i];
-        //         continue;
-        //     }
-        //     if(i!=n-1 and i!=0 and prices[i]>prices[i+1] and prices[i]>prices[i-1] and buy != -1 and sell == -1){
-        //         sell = prices[i];
-        //         amount += (sell-buy);
-        //         buy = -1;
-        //         sell = -1;
-        //         continue;
-        //     }
-        //     if(i==n-1 and buy != -1 and prices[i-1]<prices[i]){
-        //         sell = prices[i];
-        //         amount += (sell-buy);
-        //         buy = -1;
-        //         sell = -1;
-        //         continue;
-        //     }
-        // }
-        // return amount;
     }
 };
