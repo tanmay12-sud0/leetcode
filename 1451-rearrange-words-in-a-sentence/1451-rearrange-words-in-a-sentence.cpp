b0e3e1100974e5be4a04ad8034c917b15ea32797
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-02-05 09:47:28 +0530

    Time: 37 ms (53.98%), Space: 13.3 MB (89.68%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.cpp
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.cpp
@@ -1,40 +1,70 @@
-bool compare(const pair<string, int>&a, const pair<string, int>&b){
-    if(a.first.length() == b.first.length()){
-        return a.second < b.second;
-    }
-    return a.first.length()<b.first.length();
-}
-
 class Solution {
 public:
-    string arrangeWords(string s) {
-        vector<pair<string, int>> hash;
-        char* str = new char[s.length() + 2];
-        strcpy(str, s.c_str());
-        s = "";
-        char strDel[] = " ";
-        int index = 0;
-        char* token = strtok(str, strDel);
-        s += token;
-        s[0] += 32;
-        hash.push_back(make_pair(s, index++));
-        while (token) {
-              token = strtok(NULL, strDel);
-              if(token != NULL){
-                  s += token;
-                  hash.push_back(make_pair(token, index++));
-              }
+    string arrangeWords(string text) {
+        text[0] = tolower(text[0]);
+        string currWord;
+        stringstream ss(text);
+        map<int, string>hash;
+        while(ss >> currWord){
+            hash[currWord.length()] += currWord + " ";
         }
-
-        delete [] str;
-        sort(hash.begin(), hash.end(), compare);
-        s = "";
+        string res;
         for(auto x : hash){
-            s += x.first;
-            s += " ";
+            res += x.second;
         }
-        s.erase(s.length()-1, 1);
-        s[0] -= 32;
-        return s;
+        res.pop_back();
+        res[0] = toupper(res[0]);
+        return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+// bool compare(const pair<string, int>&a, const pair<string, int>&b){
+//     if(a.first.length() == b.first.length()){
+//         return a.second < b.second;
+//     }
+//     return a.first.length()<b.first.length();
+// }
+
+// class Solution {
+// public:
+//     string arrangeWords(string s) {
+//         vector<pair<string, int>> hash;
+//         char* str = new char[s.length() + 2];
+//         strcpy(str, s.c_str());
+//         s = "";
+//         char strDel[] = " ";
+//         int index = 0;
+//         char* token = strtok(str, strDel);
+//         s += token;
+//         s[0] += 32;
+//         hash.push_back(make_pair(s, index++));
+//         while (token) {
+//               token = strtok(NULL, strDel);
+//               if(token != NULL){
+//                   s += token;
+//                   hash.push_back(make_pair(token, index++));
+//               }
+//         }
+
+//         delete [] str;
+//         sort(hash.begin(), hash.end(), compare);
+//         s = "";
+//         for(auto x : hash){
+//             s += x.first;
+//             s += " ";
+//         }
+//         s.erase(s.length()-1, 1);
+//         s[0] -= 32;
+//         return s;
+//     }
+// };
