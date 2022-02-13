5eb1c6873d3b8e1b7e3d9aa095cc90943e540e90
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-02-13 08:34:49 +0530

    Time: 68 ms (20.73%), Space: 18.8 MB (45.52%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& first, vector<vector<int>>& second) {
-        int n = first.size();
-        vector<vector<int>>umap;
-        for(int i=0; i<n; i++){
-            int a = first[i][0];
-            int b = first[i][1];
-            for(int j=0; j<second.size(); j++){
-                if(a<=second[j][1] and  b>= second[j][0]){
-                    int x = max(a, second[j][0]);
-                    int y = min(b, second[j][1]);
-                    umap.push_back({x, y});
-                }
-            }
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> res;
+        for(int i = 0, j = 0; i < A.size() && j < B.size();) {
+	        int lo = max(A[i][0], B[j][0]), hi = min(A[i][1], B[j][1]);
+	        if(lo <= hi) res.push_back({lo, hi});
+	        if(hi == A[i][1]) i++;
+	        else j++;
         }
-        return umap;
+        return res;
     }
 };
+
+
+// int n = first.size();
+        // vector<vector<int>>umap;
+        // for(int i=0; i<n; i++){
+        //     int a = first[i][0];
+        //     int b = first[i][1];
+        //     for(int j=0; j<second.size(); j++){
+        //         if(a<=second[j][1] and  b>= second[j][0]){
+        //             int x = max(a, second[j][0]);
+        //             int y = min(b, second[j][1]);
+        //             umap.push_back({x, y});
+        //         }
+        //     }
+        // }
+        // return umap;
