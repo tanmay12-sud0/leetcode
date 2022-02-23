a391abe4ddc3528f7920cf61654ddc82b41ccdbc
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-02-23 10:46:07 +0530

    Time: 15 ms (67.63%), Space: 8.6 MB (20.61%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,50 +1,13 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<string>st;
-        int n = s.length();
-        for(int i=0; i<n; i++){
-            if(st.empty()){
-                string tt;
-                tt += s[i];
-                st.push(tt);
-            }else{
-                if(s[i] == 'a'){
-                    string tt;
-                    tt += s[i];
-                    st.push(tt);
-                }else if(s[i] == 'b'){
-                    string a = st.top();
-                    st.pop();
-                    if(a[a.length()-1] == 'a'){
-                        a += "b";
-                        st.push(a);
-                    }else{
-                        string tt;
-                        tt += s[i];
-                        st.push(tt);
-                    }
-                }else{
-                    string a = st.top();
-                    st.pop();
-                    if(a[a.length()-1] == 'b'){
-                        if(a.size()!=2){
-                            string tt;
-                            tt += s[i];
-                            a += tt;
-                            st.push(a);
-                        }else{
-                            continue;    
-                        }
-                    }else{
-                        string tt;
-                        tt += s[i];
-                        st.push(tt);
-                    }
-                }
-            }
-        }
-        cout << st.size() << endl;
-        return !st.size();
+        bool isValid(string s) {
+    vector<char> st;
+    for (auto ch : s)
+        if (ch == 'c' && st.size() > 1 && st.back() == 'b' && st[st.size() - 2] == 'a')
+            st.resize(st.size() - 2);
+        else
+            st.push_back(ch);
+    return st.empty();
+
     }
 };
