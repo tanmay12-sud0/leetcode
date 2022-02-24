93b23679426d6e59704bc00179d63f823f11886f
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-02-24 21:56:10 +0530

    Time: 103 ms (78.27%), Space: 30.1 MB (66.97%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,54 @@
+
+class Solution {
+public:
+    
+    ListNode* merge(ListNode* a, ListNode* b){
+        if(a == NULL){
+            return b;
+        }
+        if(b == NULL){
+            return a;
+        }
+        ListNode* c;
+        if(a->val < b->val){
+            c = a;
+            c->next = merge(a->next, b);
+        }else{
+            c = b;
+            c->next = merge(a, b->next);
+        }
+        return c;
+    }
+    
+    
+    ListNode* midPoint(ListNode* head){
+        if(head==NULL or head->next == NULL){
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=NULL and fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    
+    
+    
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL or head->next == NULL){
+            return head;
+        }
+        ListNode* mid = midPoint(head);
+        ListNode* a = head;
+        ListNode* b = mid->next;
+        mid->next = NULL;
+        
+        a = sortList(a);
+        b = sortList(b);
+        
+        ListNode* c = merge(a, b);
+        return c;
+    }
+};
