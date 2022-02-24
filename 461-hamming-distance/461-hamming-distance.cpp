bb6bcd00328afd1d993f3725885fa21a8bb2f4aa
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-02-24 22:02:26 +0530

    Time: 3 ms (26.64%), Space: 5.9 MB (27.30%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int cnt = 0;
+        while(x or y){
+            int a = x&1;
+            int b = y&1;
+            if(a != b){
+                cnt++;
+            }
+            x = x>>1;
+            y = y>>1;
+        }
+        return cnt;
+    }
+};
