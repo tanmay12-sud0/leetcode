ed88c41c290a237c7406edb99db3c65107c458f4
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-02-25 12:26:27 +0530

    Time: 168 ms (46.82%), Space: 96.2 MB (86.13%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -0,0 +1,95 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+    int row = grid.size();
+	int col = grid[0].size();
+	int ans = 0;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j] == 1){
+                    ans += 4;
+                    if(j+1<col and grid[i][j+1] == 1){
+                        ans--;
+                    }
+                    if(j-1>=0 and grid[i][j-1]==1){
+                        ans--;
+                    }
+                    if(i+1<row and grid[i+1][j] == 1){
+                        ans--;
+                    }
+                    if(i-1>=0 and grid[i-1][j] == 1){
+                        ans--;
+                    }
+                }
+            }
+        }
+	
+	return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(int i=0; i<row; i++){
+// 		for(int j=0; j<col; j++){
+// 			if(grid[i][j] == 1){
+// 				ans += 4;
+// 				if(i-1>=0 and grid[i-1][j] == 1){
+// 					ans--;
+// 				}
+// 				if(i+1< row and grid[i+1][j] == 1){
+// 					ans--;
+// 				}
+// 				if(j-1>=0 and grid[i][j-1]==1){
+// 					ans--;
+// 				}
+// 				if(j+1<col and grid[i][j+1]==1){
+// 					ans--;
+// 				}
+// 			}
+// 		}
+// 	}
