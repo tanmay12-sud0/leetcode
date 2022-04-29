d01571f10386024636d5ef3c48a2f47f2044bfc9
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-04-29 14:23:39 +0530

    Time: 74 ms (6.73%), Space: 27.3 MB (5.02%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    
+    class Graph{
+public:
+	int v;
+	list<int>*l;
+
+	Graph(int n){
+		v = n;
+		l = new list<int>[v];
+			}
+
+	void addEdge(int x, int y, bool undir = true){
+	l[x].push_back(y);
+	
+	if(undir){
+		l[y].push_back(x);
+	}
+		return;
+	}
+
+
+
+	bool bipartite_graph(int node, vector<int>&visited, int parent, int color){
+		visited[node] = color;
+		
+		for(auto nbr : l[node]){
+			if(!visited[nbr]){
+				bool tanmay = bipartite_graph(nbr, visited, node, 3-color);
+				if(!tanmay){
+					return false;
+				}
+			}else if(nbr != parent && color == visited[nbr]){
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	bool check_bipartite_graph(int node){
+		vector<int>visited(v,0);
+		int parent = -1;
+        
+        for(int i=0;i<v;i++){
+             if(visited[i]==0 && bipartite_graph(i,visited,-1,1)==false){ 
+	                return false;
+	            }
+        }
+        return 1;
+	}
+
+
+};
+  
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        Graph g(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<graph[i].size(); j++){
+                g.addEdge(i, graph[i][j]);
+            }
+        }
+        return (g.check_bipartite_graph(0));
+        
+    }
+};
