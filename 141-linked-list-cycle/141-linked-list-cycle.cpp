e848aedd979c8874a162a0ec29f400de6ee0939f
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-06-12 13:20:00 +0530

    Time: 22 ms (21.89%), Space: 8 MB (57.65%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -2,15 +2,37 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, int>hash;
+        if(head == NULL){
+            return false;
+        }
         ListNode* p = head;
-        while(p != NULL){
-            if(hash.count(p)){
+        ListNode* q = head;
+        
+        while(q != NULL and q->next != NULL){
+            p = p->next;
+            q = q->next;
+            q = q!=NULL ? q->next : q;
+            if(q == p){
                 return true;
             }
-            hash[p]++;
-            p = p->next;
         }
         return false;
     }
 };
+
+
+
+
+// unorderd_map way to tackel this problem
+
+
+// unordered_map<ListNode*, int>hash;
+//         ListNode* p = head;
+//         while(p != NULL){
+//             if(hash.count(p)){
+//                 return true;
+//             }
+//             hash[p]++;
+//             p = p->next;
+//         }
+//         return false;
