54ef04eeb3fb9d6e99009fc404be96d9758eaeab
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-07-17 09:45:07 +0530

    Time: 7 ms (69.47%), Space: 13.1 MB (65.05%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -4,10 +4,8 @@ public:
     map<int, vector<pair<int,int>>> mp;
 
     void vertOrder(TreeNode *node, int x, int y){
-        
         if(node == NULL)
             return;
-        
         mp[x].push_back({node->val, y});
         vertOrder(node->left, x-1, y-1);
         vertOrder(node->right, x+1, y-1);
@@ -29,17 +27,14 @@ public:
         vertOrder(root, 0, 0);
         
         for(auto it = mp.begin(); it!=mp.end(); it++){
-            
             vector<pair<int, int>> vec = it->second;
             sort(vec.begin(), vec.end(), compare);
-            
             vector<int> ans;
             for(int i=0; i<vec.size(); i++){
                 ans.push_back(vec[i].first);
             }
             res.push_back(ans);
         }
-        
         return res;
     }
 };
