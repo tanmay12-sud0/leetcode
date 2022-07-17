a135f63585976533bc9623a980dd410dc6c4a073
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-07-17 10:46:52 +0530

    Time: 33 ms (97.76%), Space: 27.6 MB (12.32%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(binary_search(nums.begin(), nums.end(), target)){
+            auto x = lower_bound(nums.begin(), nums.end(), target);
+            return x-nums.begin();
+        }else{
+            return -1;
+        }
+        return -1;
+    }
+};
