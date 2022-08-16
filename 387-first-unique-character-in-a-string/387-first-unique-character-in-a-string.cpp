daf152c8ca83fda75759f0420515a1d7c239a43f
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-08-16 20:27:12 +0530

    Time: 55 ms (52.92%), Space: 20.3 MB (15.57%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+     
+        queue<pair<char, int>>q;
+	    int freq[27]={0};
+
+	    for(int i=0; s[i]!='\0'; i++){
+		q.push(make_pair(s[i], i));
+		int idx = s[i]-'a';
+		freq[idx]++;
+	}	
+
+	while(!q.empty()){
+			pair<char, int> f = q.front();
+			int idx = f.first-'a';
+			if(freq[idx] > 1){
+				q.pop();
+			}else{
+				return f.second ;
+				
+			}
+	}
+	if(q.empty()){
+		return -1;
+	}
+    return -1;
+    }
+};
