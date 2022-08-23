fa3494153437e97f59709ea0221e4b197a1474d1
tanmay12-sud0 <74183172+tanmay12-sud0@users.noreply.github.com>
2022-08-23 09:57:14 +0530

    Time: 363 ms (53.45%), Space: 117.9 MB (59.47%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,59 @@
+
+class Solution {
+public:
+    
+    void reverse(ListNode* &head){
+	ListNode* c = head;
+	ListNode* p = NULL;
+	ListNode* n;
+
+	while(c!=NULL){
+		n = c->next;
+		c->next = p;
+		p = c;
+		c = n;
+	}
+	head = p;
+	return;
+}
+
+ListNode* midPoint(ListNode* head){
+	if(head == NULL or head->next == NULL){
+		return head;
+	}
+	ListNode* slow = head;
+	ListNode* fast = head->next;
+
+	while(fast!= NULL and fast->next!= NULL){
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return slow;
+}
+
+ 
+    
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL or head->next == NULL){
+		return true;
+	}
+	ListNode* mid = midPoint(head);
+	ListNode* temp = mid->next;
+	mid->next = NULL;
+	reverse(temp);
+	mid->next = temp;
+	
+	ListNode* first = head;
+	ListNode* second = mid->next;
+
+	while(second!=NULL){
+		
+		if(first->val != second->val){
+			return false;
+		}
+		first = first->next;
+		second = second->next;
+	}
+	return true;
+    }
+};
